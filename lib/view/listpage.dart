b3557fce37fd896ca16ae1df6e96d813e8d30f62
CSP2020-NOTE4\iwase <kojiko430430@gmail.com>
2022-07-02 20:03:04 +0900

    listpageデモ追加

--- lib/view/listpage.dart
+++ lib/view/listpage.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
 
+
 class ListPage extends StatelessWidget {
   const ListPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Card(
+                  margin:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    title: Text(
+                      'abc',
+                      style: TextStyle(
+                        fontFamily: 'SyneMono',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  )),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  title: Text(
+                    'kamoku',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                ),
+              )
 
-
-
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
